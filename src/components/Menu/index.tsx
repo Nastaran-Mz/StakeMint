23fe2641a2a763d68cad51404408d28ab938360f
Nastaran <mazaherinastaran@gmail.com>
2023-04-19 18:33:05 +0430

    new view for dark and light

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,151 @@
+import React, { useRef } from 'react'
+import {
+  Info, BookOpen, Code, 
+  Moon,
+  Sun,
+} from 'react-feather'
+import styled from 'styled-components'
+import { ReactComponent as MenuIcon } from '../../assets/images/menu.svg'
+import { useOnClickOutside } from '../../hooks/useOnClickOutside'
+import useToggle from '../../hooks/useToggle'
+import { useDarkModeManager } from '../../state/user/hooks'
+
+
+import { ExternalLink } from '../../theme'
+
+const StyledMenuIcon = styled(MenuIcon)`
+  path {
+    stroke: ${({ theme }) => theme.text1};
+  }
+`
+
+const StyledMenuButton = styled.button`
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: transparent;
+  margin: 0;
+  padding: 0;
+  height: 35px;
+  background-color: ${({ theme }) => theme.bg3};
+
+  padding: 0.15rem 0.5rem;
+  border-radius: 0.5rem;
+
+  :hover,
+  :focus {
+    cursor: pointer;
+    outline: none;
+    background-color: ${({ theme }) => theme.bg4};
+  }
+
+  svg {
+    margin-top: 2px;
+  }
+`
+
+const StyledMenu = styled.div`
+  margin-left: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  border: none;
+  text-align: left;
+`
+
+const MenuFlyout = styled.span`
+  min-width: 8.125rem;
+  background-color: ${({ theme }) => theme.bg3};
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
+    0px 24px 32px rgba(0, 0, 0, 0.01);
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  position: absolute;
+  top: 3rem;
+  right: 0rem;
+  z-index: 100;
+`
+
+const MenuItem = styled(ExternalLink)`
+  flex: 1;
+  padding: 0.5rem 0.5rem;
+  color: ${({ theme }) => theme.text2};
+  :hover {
+    color: ${({ theme }) => theme.text1};
+    cursor: pointer;
+    text-decoration: none;
+  }
+  > svg {
+    margin-right: 8px;
+  }
+`
+
+
+const ToggleMenuItem = styled.button`
+  background-color: transparent;
+  margin: 0;
+  padding: 0;
+  border: none;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.5rem 0.5rem;
+  justify-content: space-between;
+  font-size: 1rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text2};
+  :hover {
+    color: ${({ theme }) => theme.text1};
+    cursor: pointer;
+    text-decoration: none;
+  }
+`
+
+// const CODE_LINK = 'https://github.com/Uniswap/uniswap-interface'
+
+export default function Menu() {
+  const node = useRef<HTMLDivElement>()
+  const [open, toggle] = useToggle(false)
+
+  useOnClickOutside(node, open ? toggle : undefined)
+
+  const [darkMode, toggleDarkMode] = useDarkModeManager()
+
+
+  return (
+    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
+    <StyledMenu ref={node as any}>
+      <StyledMenuButton onClick={toggle}>
+        <StyledMenuIcon />
+      </StyledMenuButton>
+      {open && (
+        <MenuFlyout>
+        
+          <ToggleMenuItem onClick={() => toggleDarkMode()}>
+            <div>{darkMode ? <h6>Light Theme</h6> : <h6>Dark Theme</h6>}</div>
+            {darkMode ? <Moon opacity={0.6} size={16} /> : <Sun opacity={0.6} size={16} />}
+          </ToggleMenuItem>
+
+          <MenuItem id="link" href="#">
+            <Info size={14} />
+            Swap
+          </MenuItem>
+          <MenuItem id="link" href="#">
+            <BookOpen size={14} />
+            Tokens
+          </MenuItem>
+          <MenuItem id="link" href="#">
+            <Code size={14} />
+            Pools
+          </MenuItem>
+    
+        </MenuFlyout>
+      )}
+    </StyledMenu>
+  )
+}
